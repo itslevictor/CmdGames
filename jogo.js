a4eadf0d9b5eef4f30e04f8a810805fb9fb40b09
itslevictor <adm.victorfernando@gmail.com>
2023-08-16 09:03:18 -0300

    Adição do inicio do jogo em Javascript

---
+++ jogo.js
@@ -0,0 +1,52 @@
+let gameLoop = true;
+let contador = 0;
+//A cada 61 contadores, temos 1 frame para ficar 60fps
+
+while (gameLoop){
+    let segundos = parseInt(contador/3660);
+    //inicio do jogo
+    contador++
+    if ((contador >= 0) && (contador < 61)){
+
+    }
+    if ((contador >= 61) && (contador < 122)){
+        
+    }
+    if ((contador >= 122) && (contador < 183)){
+        
+    }
+    if ((contador >= 183) && (contador < 244)){
+        
+    }
+    if ((contador >= 244) && (contador < 305)){
+        
+    }
+    if ((contador >= 305) && (contador < 366)){
+        
+    }
+    if ((contador >= 366) && (contador < 427)){
+        
+    }
+    if ((contador >= 427) && (contador < 488)){
+        
+    }
+    if ((contador >= 488) && (contador < 549)){
+        
+    }
+    if ((contador >= 549) && (contador < 610)){
+        
+    }
+    if ((contador >= 610) && (contador < 671)){
+        
+    }
+    if ((contador >= 671) && (contador < 732)){
+        
+    }
+    if ((contador >= 732) && (contador < 793)){
+        
+    }
+    else if (contador>=3660){
+        contador = 0;
+    }
+    console.clear();
+}
