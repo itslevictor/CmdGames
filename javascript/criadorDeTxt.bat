e0678a530eea011a5dd84782c79f77b5bac0433d
Victor <adm.victorfernando@gmail.com>
2023-08-18 08:51:51 -0300

    Update criadorDeTxt.bat

--- javascript/criadorDeTxt.bat
+++ javascript/criadorDeTxt.bat
@@ -2,3 +2,5 @@
 for /L %%i in (1,1,12) do (
   echo. > C:\Users\vande\Documents\CmdGames\javascript\SPRITES\sprite%%i.txt
 )
+# abrir windows explorer
+# explorer.exe /e, /n,c:\Usuários/vande/documents
